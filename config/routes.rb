48787d04bb496128179ad54af4106aa3ff57932c
naoko kasahara <kassa0918@gmail.com>
2022-02-19 00:49:22 +0900

    bootstrap, haml, fontawesome追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resource :top, only: %i[show]
+  root 'tops#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
